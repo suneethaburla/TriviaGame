bf9cd5139ce1799985fdded3b9181a1e398987ae
Satya Bantupalli <sbantupalli@Satyas-MacBook-Pro.local>
2017-11-26 12:08:10 +0530

    This is first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,194 @@
+$(document).ready(function() {
+	var index = 0;
+	var countdownTimer = {
+		time : 30,
+		reset: function() {
+			this.time = 30;
+			$('.timer').html('<h3>Time remaining: ' + this.time + 'seconds</h3>');
+		},
+		start: function() {
+			counter = setInterval(countdownTimer.count, 1000);	
+		},
+		stop: function() {
+			clearInterval(counter);
+		},
+		count: function() {
+				countdownTimer.time--;
+				console.log(countdownTimer.time);
+			if (countdownTimer.time >= 0) {
+				$('.timer').html('<h3>Time remaining: ' + countdownTimer.time + ' seconds</h3>');
+			}
+			else {
+				index++;
+				answerWrong();
+				countdownTimer.reset();
+				if (index < questionArray.length) {
+					loadQuestion(index);
+				} else {
+					$(".answerchoice").hide();
+					showScore();
+				}
+			}
+		}
+	};
+
+var correct = 0;
+var wrong = 0;
+var q1 = {
+	question : 'What does CSS stand for?',
+	possibleAnswers : ['A. Colorful Style Sheets',
+				 'B. Creative Style Sheets',
+				 'C. Cascading Style Sheets',
+				 'D. Computer Style Sheets'],
+	flags : [false, false, true, false],
+	answer : 'C. Cascading Style Sheets'
+};
+
+var q2 = {
+	question: 'How do you select an element with id "demo"?',
+	possibleAnswers: ['A. .demo',
+				 'B. #demo',
+				 'C. *demo',
+				 'D. demo:'],
+	flags : [false, true, false, false],
+	answer : 'B. #demo'
+};
+
+var q3 = {
+	question : 'What is the default value of the position property?',
+	possibleAnswers : ['A. fixed',
+				 'B. Static',
+				 'C. relative',
+				 'D. absolute'],
+	flags : [false, true, false, false],
+	answer : 'B. Static'
+};
+
+var q4 = {
+	question : 'What does HTML stand for?',
+	possibleAnswers : ['A. Hyper Text Markup Language',
+				 'B. Homepage Tab Markup Language',
+				 'C. HyperLinks and text Markup Language',
+				 'D. HyperLink Table Must Language'],
+	flags : [true, false, false, false],
+	answer : 'A. Hyper Text Markup Language'
+};
+
+var q5 = {
+	question : 'How can you make a numbered list?',
+	possibleAnswers : ['A. <list>',
+				 'B. <ol>',
+				 'C. <nl>',
+				 'D. <ul>'],
+	flags : [false, true, false, false],
+	answer : 'B. <ol>'
+};
+
+var questionArray = [q1, q2, q3, q4, q5];
+
+function loadQuestion(questionSelection) {
+	console.log(questionSelection);
+	countdownTimer.reset();
+  $(".question").html("<h3>" + questionArray[questionSelection].question + "</h3>");
+  $("#buttonA").text(questionArray[questionSelection].possibleAnswers[0]).show();
+  $("#buttonB").text(questionArray[questionSelection].possibleAnswers[1]).show();
+  $("#buttonC").text(questionArray[questionSelection].possibleAnswers[2]).show();
+  $("#buttonD").text(questionArray[questionSelection].possibleAnswers[3]).show();
+}
+
+
+function setup() {
+	index = 0;
+	$('.question').append('<button id="startButton">Start</button>');
+	$('#startButton').on('click', function() {
+		$(this).hide();
+		countdownTimer.start();
+	 	loadQuestion(index);
+	});
+}		
+
+function getAnswer() {
+
+
+	$('.answerchoice').on('click', function() {
+	  console.log('alert', index);
+		index++;
+		console.log('click', index);
+		$(".question").text('');
+		$("#buttonA").text('');
+		$("#buttonB").text('');
+		$("#buttonC").text('');
+		$("#buttonD").text('');
+		loadQuestion();
+	})
+}
+
+function answerCorrect() {
+	correct++;
+	alert("Correct!");
+	console.log("correct");
+}
+
+function answerWrong() {
+	wrong++;
+	alert("Incorrect!");
+	console.log("wrong");
+}
+
+function showScore() {
+	$('.question').empty();
+	$('.question').append("<h2><p>" + correct + " correct</p></h2>");
+	$('.question').append("<h2><p>" + wrong + " incorrect</p></h2>");
+	countdownTimer.stop();
+	$('.timer').empty();
+
+}
+
+setup();
+$('.answerchoice').on('click', function() {
+ console.log($(this));
+ if(this.id == 'buttonA') {
+ 	var answerChosen = 'A';
+ } else if(this.id == 'buttonB') {
+ 	answerChosen = 'B';
+ } else if (this.id == 'buttonC') {
+ 	answerChosen = 'C';
+ } else if (this.id == 'buttonD') {
+ 	answerChosen = 'D';
+ } 
+ if ((answerChosen == 'A') && (questionArray[index].flags[0] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'A') {
+ 	answerWrong();
+ }
+ if ((answerChosen == 'B') && (questionArray[index].flags[1] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'B') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'C') && (questionArray[index].flags[2] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'C') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'D') {
+ 	answerWrong();
+ }
+
+ $(".question").text('');
+ $("#buttonA").text('');
+ $("#buttonB").text('');
+ $("#buttonC").text('');
+ $("#buttonD").text('');
+ index++;
+ if (index < questionArray.length) {
+ 	loadQuestion(index);
+ } else {
+ 	$(".answerchoice").hide();
+ 	showScore();
+ }
+});
+
+});
